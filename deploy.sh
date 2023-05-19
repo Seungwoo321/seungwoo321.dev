ef7a35b725c461e1632b7b11e1ee24dc2cd5bc56
Seungwoo321 <seungwoo321@gmail.com>
2023-05-19 23:33:01 +0900

    refactor: update script master to main

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git config --local user.name "Seungwoo Lee"
 git config --local user.email "seungwoo321@gmail.com"
 git add -A
 git commit -m "chore: by deploy.sh"
-git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git master:gh-pages
+git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git main:gh-pages
 
 cd -
