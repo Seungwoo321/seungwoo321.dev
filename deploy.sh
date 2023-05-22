39f226aebb9eff91168832143192bd25d194bb75
Seungwoo321 <seungwoo321@gmail.com>
2023-05-22 11:30:17 +0900

    chore: build test

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ git config --local user.name "Seungwoo Lee"
 git config --local user.email "seungwoo321@gmail.com"
 git add -A
 git commit -m "chore: by deploy.sh"
-git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git main:gh-pages
+git remote add origin git@github.com:Seungwoo321/seungwoo321.github.io.git
+git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git maser:gh-pages
 
 cd -
